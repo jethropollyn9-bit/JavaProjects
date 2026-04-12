69c09e24345793a01e670f45e24dd2400cf50921
c25075347 <pollynjt@cardiff.ac.uk>
2026-04-12 20:16:25 +0100

    changes made to the main

--- Main.java
+++ Main.java
@@ -1,8 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        // Create two players to compare
         ProPlayer player1 = new ProPlayer("Jethro", 92, 88, 75, 82);
-        ProPlayer player2 = new ProPlayer("Transfer Target", 85, 82, 80, 78);
+        ProPlayer player2 = new ProPlayer("Rivals Scout", 80, 85, 70, 75);
 
+        // print their stats
+        System.out.println(player1);
+        System.out.println(player2);
+
+        // Run the omparison
         player1.compareTo(player2);
     }   
 }
